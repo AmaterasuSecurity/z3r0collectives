4ca23f15698bae0c0c2a0de2f37701f930e3966d
droppedpackets <56236210+dr0pp3dpack3ts@users.noreply.github.com>
2021-08-20 13:02:07 -0700

    Update # A Windows Privilege Escalation!.ps1

--- # A Windows Privilege Escalation!.ps1
+++ # A Windows Privilege Escalation!.ps1
@@ -1,14 +1,9 @@
 # A Windows Privilege Escalation!
 #This does...
-#   + Version checks, patch checking, lists all drives
+#   + OS and Version Checks
 #   + List User Privileges
 #   + Lists all users
 
-systeminfo | findstr /B /C:"OS Name" /C:"OS Version"
-wmic qfe
-whoami /priv
-whoami /groups
-net user
-whoami /all
-Get-LocalUser | Format-Table Name,Enabled,LastLogon
-Get-ChildItem C:\Users -Force | Select-Object Name
+systeminfo | findstr /B /C:"OS Name" /C:"OS Version" ### OS name and version
+Get-ChildItem C:\Users -Force | Select-Object Name ### Lists all user profiles on machine.
+net localgroup administrators ### Lists all administrator groups
