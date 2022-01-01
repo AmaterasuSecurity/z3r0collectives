7852cdfb9f475fd73566319403b9c23200a3b205
Your Name <ali18@wgu.edu>
2021-12-31 16:07:08 -0800

    commit

--- google.py
+++ google.py
@@ -1,12 +1,16 @@
-#!/opt/homebrew/bin/python3
-
-try:
-    from googlesearch import search
-except ImportError:
-    print("No module named 'google' found")
-
-# to search
-query = "geeksforgeeks"
-
-for j in search(query, tld="co.in, num=10, stop=10, pause=2"):
-    print(j)
+class Gsearch_python:
+    def__init__(self,name_search):
+        self.name = name_search
+    def Gsearch(self):
+        count = 0
+        try:
+            from googlesearch import search
+        except ImportError:
+            print("No Module named 'Google' found!")
+        for i in search(query=self.name, tld='co.in', lang='en', num=10, stop=1 pause=2):
+            count += 1
+            print (count)
+            print(i + '\n')
+if __name__=='__main__':
+    gs = Gsearch_python()
+    gs.Gsearch()
